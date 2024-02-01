3dd620eddfe15a824bfeadd1c35db0cfbf856c75
Valquiria Pereira dos Santos <valquiriapereiradossantos@gmail.com>
2024-02-01 14:47:14 -0300

    iniciando o javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+const $buttonStonePlayer1 = document.querySelector('.button-move-stone-1')
+
+function handleStone1Move(){
+alert('clicou na pedra 1')
+}
+
+$buttonStonePlayer1.addEventListener('click', handleStone1Move)
